afa1e81899851be2cc0225a4d5b414d84fe96499
Jayson Ragasa <alfonsojayson.ragasa@infor.com>
2026-06-14 11:57:14 +0800

    feat: implement hardware abstraction layer for PCA9685 PWM servo control

--- src/HAL.ino
+++ src/HAL.ino
@@ -56,9 +56,8 @@ void updateLegs() {
 
 double limitServoAngle(double angle)
 {
-  // TODO this is not right solution, we should restart calc to solve the angle instead of just limit if something wrong
-  //      also this is nothing about real mechanical limits, so need TODO
-  
+  if (isnan(angle)) return -1; // Special flag to indicate math failure
+
   // default servo can handle angles from 0 to 180 (PI) degrees only
   if (angle < 0) angle = 0;
   if (angle > PI) angle = PI;
--- src/HAL_PCA9685.ino
+++ src/HAL_PCA9685.ino
@@ -5,10 +5,11 @@ double pwmTicksPerUs = 0;
 void initServoHAL() {
   pwm = Adafruit_PWMServoDriver();
   pwm.begin();
-  pwm.setOscillatorFrequency(27000000);  // The int.osc. is closer to 27MHz
-  pwm.setPWMFreq(SERVO_FREQ);  // This is the maximum PWM frequency of servo
-  
-  // Cache the ticks-per-microsecond multiplier to avoid doing an I2C read inside angleToPulse
+  pwm.setOscillatorFrequency(27000000); // The int.osc. is closer to 27MHz
+  pwm.setPWMFreq(SERVO_FREQ); // This is the maximum PWM frequency of servo
+
+  // Cache the ticks-per-microsecond multiplier to avoid doing an I2C read
+  // inside angleToPulse
   double pulselength = 1000000.0;
   uint16_t prescale = pwm.readPrescale();
   prescale += 1;
@@ -18,25 +19,40 @@ void initServoHAL() {
 }
 
 uint16_t angleToPulse(double angleRad) {
+  if (angleRad < 0)
+    return 0; // Catch invalid angle flag from limitServoAngle
   // Map angle expected between 0 and PI to pulse width in microseconds
-  double pulse_us = mapf(angleRad, 0, M_PI, SERVO_MIN, SERVO_MAX);
+  double pulse_us = SERVO_MIN + (angleRad / M_PI) * (SERVO_MAX - SERVO_MIN);
   return pulse_us * pwmTicksPerUs;
+  // return pulse_us;
 }
 
-void setLegPWM(leg &_leg)
-{
-  pwm.setPWM(_leg.hal.pin.alpha,  0,  angleToPulse(limitServoAngle(getHALAngle(_leg.angle.alpha, _leg.hal.mid.alpha, _leg.hal.trim.alpha, _leg.hal.ratio.alpha, _leg.inverse.alpha))));
-  pwm.setPWM(_leg.hal.pin.beta,   0,  angleToPulse(limitServoAngle(getHALAngle(_leg.angle.beta,  _leg.hal.mid.beta,  _leg.hal.trim.beta,  _leg.hal.ratio.beta,  _leg.inverse.beta ))));
-  pwm.setPWM(_leg.hal.pin.gamma,  0,  angleToPulse(limitServoAngle(getHALAngle(_leg.angle.gamma, _leg.hal.mid.gamma, _leg.hal.trim.gamma, _leg.hal.ratio.gamma, _leg.inverse.gamma))));
+void setPWMIfChanged(uint8_t pin, uint16_t pulse) {
+  if (pulse == 0)
+    return; // Prevent shutting off servo due to IK math NaN
+  pwm.setPWM(pin, 0, pulse);
 }
 
-void runServoCalibrate(leg &_leg)
-{
+void setLegPWM(leg &_leg) {
+  setPWMIfChanged(_leg.hal.pin.alpha,
+                  angleToPulse(limitServoAngle(getHALAngle(
+                      _leg.angle.alpha, _leg.hal.mid.alpha, _leg.hal.trim.alpha,
+                      _leg.hal.ratio.alpha, _leg.inverse.alpha))));
+  setPWMIfChanged(_leg.hal.pin.beta,
+                  angleToPulse(limitServoAngle(getHALAngle(
+                      _leg.angle.beta, _leg.hal.mid.beta, _leg.hal.trim.beta,
+                      _leg.hal.ratio.beta, _leg.inverse.beta))));
+  setPWMIfChanged(_leg.hal.pin.gamma,
+                  angleToPulse(limitServoAngle(getHALAngle(
+                      _leg.angle.gamma, _leg.hal.mid.gamma, _leg.hal.trim.gamma,
+                      _leg.hal.ratio.gamma, _leg.inverse.gamma))));
+}
+
+void runServoCalibrate(leg &_leg) {
   uint16_t pulse = angleToPulse(M_PI_2);
-  pwm.setPWM(_leg.hal.pin.alpha, 0, pulse);
-  pwm.setPWM(_leg.hal.pin.beta,  0, pulse);
-  pwm.setPWM(_leg.hal.pin.gamma, 0, pulse);
+  setPWMIfChanged(_leg.hal.pin.alpha, pulse);
+  setPWMIfChanged(_leg.hal.pin.beta, pulse);
+  setPWMIfChanged(_leg.hal.pin.gamma, pulse);
 }
-
 
 #endif
